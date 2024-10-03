fef16dcee8807eb04cfa6aea2a467a6fc598634d
Emilia Bieniek <emilahaligowska@gmail.com>
2024-10-03 13:48:33 +0200

    Added tests for: - Valid client's recipe creation - List view

--- recipies/tests.py
+++ recipies/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Recipe
+from django.urls import reverse_lazy
 
-# Create your tests here.
+def create_recipe(title, ingredients, instruction):
+    r = Recipe.objects.create(title=title)
+    r.ingredients = ingredients
+    r.instruction = instruction
+    r.save()
+    
+class CreateRecipeTests(TestCase):
+    def test_valid_creation(self):
+        self.assertEqual(Recipe.objects.count(), 0)
+        client = Client()
+        data = {'title':"Kanapka", "ingredients": "chleb, ser", "instruction":"Poloz ser na chlebie"}
+        response = client.post(reverse_lazy('recipies:create'), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('recipies:list'))
+        self.assertEqual(Recipe.objects.count(), 1)
+        
+class ViewsTest(TestCase):
+    def test_list_view(self):
+        client = Client()
+        response = client.get(reverse_lazy('recipies:list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,"No recipies yet.")
+        
+        create_recipe("Owsianka", "mleko, platki owsiane", "Dodaj platki do mleka, zagotuj.")
+        response = client.get(reverse_lazy('recipies:list'))
+        self.assertEqual(response.status_code, 200)    
+        self.assertNotContains(response, "No recipies yet.")
