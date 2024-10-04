d31de04d2338c0d32fff6bc252a4611f646e960c
Emilia Bieniek <emilahaligowska@gmail.com>
2024-10-04 12:42:46 +0200

    Code formatting after running lint.

--- recipies/models.py
+++ recipies/models.py
@@ -4,6 +4,6 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.TextField(max_length=200)
     instruction = models.TextField(max_length=500)
-    
+
     def __str__(self):
         return self.title
--- recipies/views.py
+++ recipies/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
 from django.views import generic
-from .models import Recipe
 from django.urls import reverse_lazy
+from .models import Recipe
 
 class IndexView(generic.ListView):
     model = Recipe
 
 class DetailView(generic.DetailView):
     model = Recipe
-    
+
 class CreateView(generic.edit.CreateView):
     model = Recipe
     fields = ["title","ingredients", "instruction"]
     success_url = reverse_lazy('recipies:list')
-    
