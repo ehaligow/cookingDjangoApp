39138092d8840f5d1578569daf1ea312797694b9
Emilia Bieniek <emilahaligowska@gmail.com>
2024-10-08 09:13:05 +0200

    Added models Category and Ingredient.

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    kcal_per_100g = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- recipies/tests.py
+++ recipies/tests.py
@@ -1,21 +1,26 @@
 from django.test import TestCase, Client
 from django.urls import reverse_lazy
 from .models import Recipe
+from ingredients.models import Category, Ingredient
 
-def create_recipe(title, ingredients, instruction):
-    r = Recipe.objects.create(title=title)
-    r.ingredients = ingredients
-    r.instruction = instruction
+def create_recipe(title):
+    i1, i2 = create_ingedients()
+    r = Recipe.objects.create(title=title, instruction="Posmaruj bulke maslem.")
+    r.ingredients.add(i1)
+    r.ingredients.add(i2)
     r.save()
+
+def create_ingedients():
+    c1 = Category.objects.create(name="pieczywo")
+    i1 = Ingredient.objects.create(name="bulka", category_id = c1.id)
+    c2 = Category.objects.create(name="nabial")
+    i2 = Ingredient.objects.create(name="serek", category_id = c2.id)
+    return i1, i2
 
 class CreateRecipeTests(TestCase):
     def test_valid_creation(self):
         self.assertEqual(Recipe.objects.count(), 0)
-        client = Client()
-        data = {'title':"Kanapka", "ingredients": "chleb, ser", "instruction":"Poloz ser na chleb"}
-        response = client.post(reverse_lazy('recipies:create'), data)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse_lazy('recipies:list'))
+        create_recipe("bulka z maslem.")
         self.assertEqual(Recipe.objects.count(), 1)
 
 class ViewsTest(TestCase):
@@ -25,7 +30,7 @@ class ViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,"No recipies yet.")
 
-        create_recipe("Owsianka", "mleko, platki owsiane", "Dodaj platki do mleka, zagotuj.")
+        create_recipe("Owsianka")
         response = client.get(reverse_lazy('recipies:list'))
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, "No recipies yet.")
