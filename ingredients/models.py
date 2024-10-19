9a1347401d5676334566b4d2d29aac5d58bcf599
Emilia Bieniek <emilahaligowska@gmail.com>
2024-10-19 12:07:05 +0200

    Added creating shopping list.

--- ingredients/models.py
+++ ingredients/models.py
@@ -13,3 +13,10 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
--- ingredients/views.py
+++ ingredients/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.views import generic
-from .models import Ingredient
+from .models import Ingredient, ShoppingList
 
 class Add(generic.CreateView):
     fields = ["name", "kcal_per_100g", "category"]
@@ -9,3 +10,16 @@ class Add(generic.CreateView):
 
 class List(generic.ListView):
     model = Ingredient
+
+def addToShoppingList(request):
+    # TODO
+    # use get_list_or_404
+    selected_ingredients = request.POST.getlist("ingredient")
+    if not selected_ingredients:
+        print("TODO")
+    else:
+        shopping_list = ShoppingList(name="NewList")
+        shopping_list.save()
+        for ingredient in selected_ingredients:
+            shopping_list.ingredients.add(ingredient)
+    return HttpResponseRedirect(reverse_lazy("ingredients:list"))
