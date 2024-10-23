8d3fe7ca74e05b6829321ef13d200a8f8d822d21
Emilia Bieniek <emilahaligowska@gmail.com>
2024-10-23 11:51:05 +0200

    Added link to list elements on shopping lists view.

--- shoppingList/urls.py
+++ shoppingList/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ShoppingListView.as_view()),
-    path("<int:pk>/", views.ShoppingListDetail.as_view()),
+    path("<int:pk>/", views.ShoppingListDetail.as_view(), name="detail"),
 ]
