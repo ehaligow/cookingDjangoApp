e6e53782b550fbb42606fa6f5f49a2fa29754770
Emilia Bieniek <emilahaligowska@gmail.com>
2024-10-24 13:49:49 +0200

    Download list.

--- shoppingList/urls.py
+++ shoppingList/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.ShoppingListView.as_view()),
     path("<int:pk>/", views.ShoppingListDetail.as_view(), name="detail"),
+    path("<int:shoppinglist_id>/downloadList", views.downloadList, name="downloadList"),
 ]
