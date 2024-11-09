b27d52057662255ffaa86ad0702e27da31f6b54d
Emilia Bieniek <emilahaligowska@gmail.com>
2024-11-09 17:57:17 +0100

    Add category creation.

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views import generic
-from .models import Ingredient
+from .models import Ingredient, Category
 from shoppingList.models import ShoppingList
 
 class Add(generic.CreateView):
@@ -11,6 +11,12 @@ class Add(generic.CreateView):
 
 class List(generic.ListView):
     model = Ingredient
+
+class CreateCategory(generic.CreateView):
+    model = Category
+    template_name = 'ingredients/category_form.html'
+    fields = ["name"]
+    success_url = reverse_lazy("ingredients:list")
 
 def addToShoppingList(request):
     # TODO
