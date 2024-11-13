4657f7b8e2120ab1ff4ee853de2b44689cfbefa8
Emilia Bieniek <emilahaligowska@gmail.com>
2024-11-13 15:31:11 +0100

    Delete list.

--- shoppingList/urls.py
+++ shoppingList/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.ShoppingListView.as_view(), name="list"),
     path("<int:pk>/", views.ShoppingListDetail.as_view(), name="detail"),
     path("<int:shoppinglist_id>/downloadList", views.downloadList, name="downloadList"),
+    path("<int:shoppinglist_id>/delete", views.deleteList, name="delete")
 ]
