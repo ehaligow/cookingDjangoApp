1029c3c0f5113412b16306c3bb63af505fb232d2
EmiliaBieniek <emilahaligowska@gmail.com>
2024-12-09 17:50:46 +0100

    Upgrade creating shopping list (#11)
    * Create list from list view
    
    * [WIP] Update creating shopping list.
    
    * Using request session redirect chosen ingredients.
    
    * Create test for successful list creation.
    
    * Added tests for invalid creation and form view.
    
    * Added test for form view with selected ingredients.
    
    * Code refactor.

--- shoppingList/urls.py
+++ shoppingList/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name= "shoppingList"
 urlpatterns = [
     path("", views.ShoppingListView.as_view(), name="list"),
+    path("createListWithIngredients", views.add_ingredients_to_session,
+         name='createListWithIngredients'),
+    path("createList", views.create_list, name='createList'),
     path("<int:pk>/", views.ShoppingListDetail.as_view(), name="detail"),
-    path("<int:shoppinglist_id>/downloadList", views.downloadList, name="downloadList"),
+    path("<int:shoppinglist_id>/downloadList", views.download_list, name="downloadList"),
 ]
