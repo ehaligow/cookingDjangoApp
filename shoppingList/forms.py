09b2b1794115e868212122c25bd683f7c07bd3a3
ehaligow <emilahaligowska@gmail.com>
2024-12-16 15:21:51 +0100

    Accept shopping list creation with name only.

--- shoppingList/forms.py
+++ shoppingList/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from .models import ShoppingList, Ingredient
 
+
 class ShoppingListForm(forms.ModelForm):
     class Meta:
         model = ShoppingList
         fields = ['name', 'ingredients']
 
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+    )
+
     ingredients = forms.ModelMultipleChoiceField(
         queryset=Ingredient.objects.all(),
         widget=forms.CheckboxSelectMultiple,
-        required=True
+        required=False
     )
