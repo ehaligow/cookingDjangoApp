9c14bef457731e72dccee905a0adf8eb2a581303
ehaligow <emilahaligowska@gmail.com>
2024-12-19 13:12:21 +0100

    Created new app for home view.

--- src/home/urls.py
+++ src/home/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("home.urls")),
+    path("", views.home, name="home"),
+    path("ingredients/",include("ingredients.urls")),
+    path("shoppingList/", include("shoppingList.urls")),
+    path("recipies/", include("shoppingList.urls")),
 ]
