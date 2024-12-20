87528f890ad0e5c69c971fa0b353dceebba778da
ehaligow <emilahaligowska@gmail.com>
2024-12-20 11:43:48 +0100

    Code format.

--- src/recipies/views.py
+++ src/recipies/views.py
@@ -5,16 +5,20 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 
+
 class IndexView(generic.ListView):
     model = Recipe
+
 
 class DetailView(generic.DetailView):
     model = Recipe
 
+
 class CreateView(generic.edit.CreateView):
     model = Recipe
-    fields = ["title","ingredients", "instruction"]
+    fields = ["title", "ingredients", "instruction"]
     success_url = reverse_lazy('recipies:list')
+
 
 def delete(req, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
