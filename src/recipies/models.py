571ca297ce5257aaaae49b9f827321bcb3c736e1
EmiliaBieniek <emilia.haligowska@intel.com>
2025-01-20 18:51:42 +0100

    Enhance recipe app (#18)
    * Move category to recipe app.
    
    * Get random recipe from external API.
    
    * Handle more specific errors.
    
    * Address review comments and fix failing build.
    
    * Fix migrations.

--- src/recipies/models.py
+++ src/recipies/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from ingredients.models import Ingredient
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.ManyToManyField(Ingredient)
     instruction = models.TextField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- src/recipies/views.py
+++ src/recipies/views.py
@@ -1,9 +1,11 @@
 from django.views import generic
 from django.urls import reverse_lazy
-from .models import Recipe
-from django.shortcuts import get_object_or_404
+from ingredients.models import Ingredient
+from .models import Recipe, Category
+from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+import requests
 
 
 class IndexView(generic.ListView):
@@ -16,7 +18,7 @@ class DetailView(generic.DetailView):
 
 class CreateView(generic.edit.CreateView):
     model = Recipe
-    fields = ["title", "ingredients", "instruction"]
+    fields = ["title", "ingredients", "instruction", "category"]
     success_url = reverse_lazy('recipies:list')
 
 
@@ -24,3 +26,37 @@ def delete(req, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     recipe.delete()
     return HttpResponseRedirect(reverse_lazy("recipies:list"))
+
+
+class CreateCategory(generic.CreateView):
+    model = Category
+    template_name = 'category_form.html'
+    fields = ["name"]
+    success_url = reverse_lazy("recipies:list")
+
+
+def get_recipe_from_API(req):
+    try:
+        response = requests.get(
+            "https://www.themealdb.com/api/json/v1/1/random.php")
+        data = response.json()['meals'][0]
+    except (requests.exceptions.RequestException, KeyError, IndexError) as e:
+        print(f"Error during get {e}.")
+        return redirect("recipies:list")
+
+    title = data['strMeal']
+    instructions = data['strInstructions']
+    category, _ = Category.objects.get_or_create(name=data['strCategory'])
+
+    all_ingredients = [k for k in data.keys() if k.startswith('strIngredient')]
+    ingredients_names = [data[ing] for ing in all_ingredients if data[ing]]
+    ingredients = []
+    for name in ingredients_names:
+        # TODO: bulk_create or get_or_create?
+        ingredient, _= Ingredient.objects.get_or_create(name=name)
+        ingredients.append(ingredient.id)
+
+    recipe = Recipe.objects.create(
+        title=title, instruction=instructions, category=category)
+    recipe.ingredients.set(ingredients)
+    return redirect("recipies:detail", pk=recipe.id)
